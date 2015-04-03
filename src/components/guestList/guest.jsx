3fcb7bc2ec4c254478989c864ed987f6d40cfba0
Kia Fathi <kia.fathi@minted.com>
2015-04-03 14:10:10 -0700

    (feat) Create editable guest list item

--- src/components/guestList/guest.jsx
+++ src/components/guestList/guest.jsx
@@ -1,4 +1,8 @@
 var React = require('react');
+var classNames = require('classnames');
+var assign = require('object-assign');
+
+var AddressbookActions = require('../../actions/addressbookActions.js');
 
 //var EditableField = require('../common/editablField.jsx');
 
@@ -14,11 +18,22 @@ export default class Guest extends React.Component {
   constructor(props) {
     super(props);
   }
+  updateGuestDisplayName(e){
+    var newName = e.target.value;
+    var email = this.props;
+    AddressbookActions.editEmail(assign(email, {displayName: newName}));
+  }
   render(){
+    var cx = classNames(['ABGuest', 'row']);
     return (
-      <div>
-        <p>{this.props.name}</p>
-        <p>{this.props.email}</p>
+      <div className={cx}>
+        <div className='col-md-4'>{this.props.name}</div>
+        <div className='col-md-4'>{this.props.email}</div>
+        <div className='col-md-4'>
+          <input type='text'
+            onChange={this.updateGuestDisplayName.bind(this)}
+            value={this.props.displayName}/>
+        </div>
       </div>
     );
   }
